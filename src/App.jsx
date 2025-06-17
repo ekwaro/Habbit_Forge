93ed9253f622ee11401eb267ec4cf4801838fe8c
ekwaro <dominicnoelchris@gmail.com>
2025-06-17 06:27:49 +0300

    update application with routing mantine library configurations

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Center, Text } from '@mantine/core'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return(
     <>
+    <Center>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Text size="xl" weight={700} mb="md">Welcome to HabbitForge!</Text>
+        <Text size="md" c="dimmed">Your journey to better habits starts here.</Text>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </Center>
     </>
   )
 }
-
 export default App
