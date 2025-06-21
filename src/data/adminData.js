91d85b93d3e7b0408a890d0f74298182394ec375
SaulWeaponz <saulweaponz@gmail.com>
2025-06-22 00:52:05 +0300

    Add sample data

---
+++ src/data/adminData.js
@@ -0,0 +1,7 @@
+export const getAdminInfo = () => {
+  return {
+    name: "Saul S",
+    profilePicture: "https://images.pexels.com/photos/18870059/pexels-photo-18870059.jpeg",
+    email: "sauls@habitsforge.com",
+  };
+};
