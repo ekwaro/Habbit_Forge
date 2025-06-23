7408a66dcfdea754872c1c4f0742ec19f3432267
ekwaro <dominicnoelchris@gmail.com>
2025-06-23 05:32:52 +0300

    Revert "user dashboard"

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import { useState } from 'react'
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css'; //Mantine's global styles
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import { Center, Text } from '@mantine/core'
+
+const theme = createTheme({
+  primaryColor: 'teal', // Example primary color
+});
+
+
+
+import { Notifications } from '@mantine/notifications';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom'; 
+import { saveUserToStorage, removeUserFromStorage } from './utils/localStorage';
+import DashboardPage from './pages/DashboardPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  useEffect(() => {
-  if ("Notification" in window && Notification.permission !== "granted") {
-    Notification.requestPermission();
-  }
-}, []);
+  const { user, isAuthenticated } = useAuth0();
 
-  return(
-    <>
-    <Center>
-      <div>
-        <Text size="xl" weight={700} mb="md">Welcome to HabbitForge!</Text>
-        <Text size="md" c="dimmed">Your journey to better habits starts here.</Text>
-      </div>
-    </Center>
-    </>
-  )
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      saveUserToStorage(user);
+    } else if (!isAuthenticated) {
+      removeUserFromStorage();
+    }
+  }, [isAuthenticated, user]);
+
+  return (
+    <main>
+      <Outlet />
+      
+    </main>
+  );
 }
-export default App
+
+export default App;
